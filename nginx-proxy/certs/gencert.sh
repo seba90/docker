9faad49f986ab14e766d5bcec2c17f8f484cd6fe
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-06-18 16:50:23 +0200

    Updated docker stuff to fix openEASE errors

--- nginx-proxy/certs/gencert.sh
+++ nginx-proxy/certs/gencert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NAME=default
 openssl genrsa -out $NAME.key 4096
-openssl req -new -key $NAME.key -out $NAME.csr -subj "/OU=IAI/CN=localhost/O=Universitaet Bremen/C=DE"
-openssl x509 -req -days 365 -in $NAME.csr -signkey $NAME.key -out $NAME.crt
+openssl req -new -key $NAME.key -out $NAME.csr -subj "/OU=IAI/CN=localhost/O=Universitaet Bremen/C=DE" -config openssl.cnf
+openssl x509 -req -days 365 -in $NAME.csr -signkey $NAME.key -out $NAME.crt -extensions v3_req -extfile openssl.cnf
 rm $NAME.csr
